09083c670d1b67efa711c8cf62caf002ee1dc2b9
Mahamudur10 <mahamudur649@gmail.com>
2026-05-01 09:56:27 +0600

    deploy process

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://assignment-8-batch-13.vercel.app"
+    
+    // baseURL: "https://assignment-8-batch-13.vercel.app"
+    
+    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000",
+
 })
